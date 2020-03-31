8bc06f8625e8073d2c204c1a6c1be0d5be9d43d5
Felix Queißner <felix@ib-queissner.de>
2020-03-31 10:48:45 +0200

    Create docs.microsoft.js
    Remove all the auto-translated docs

---
+++ docs.microsoft.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Fixup MSDN Language
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Enforces english language on docs.microsoft.com. Auto-translated code documentation just sucks.
+// @author       Felix Queißner
+// @match        https://docs.microsoft.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.pathname = "/en-us" + window.location.pathname.substr(6);
+})();
